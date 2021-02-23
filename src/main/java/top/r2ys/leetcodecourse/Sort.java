9aeb6999155a40643ca0e2dec6ccb88aaa2f03ef
r2ys <r2ys@outlook.com>
2021-02-23 09:57:15 +0800

    add sort class.

---
+++ src/main/java/top/r2ys/leetcodecourse/Sort.java
@@ -0,0 +1,19 @@
+package top.r2ys.leetcodecourse;
+
+/**
+ * @program: leetcode-course
+ * @description:
+ * @author: HU
+ * @create: 2021-02-23 09:54
+ */
+public class Sort {
+
+    /**
+     * 排序
+     * https://mp.weixin.qq.com/s/vn3KiV-ez79FmbZ36SX9lg
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
