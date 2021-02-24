93d561bb1da1f2a9916dfa2c4ec6158f11fa0f43
r2ys <r2ys@outlook.com>
2021-02-24 23:11:06 +0800

    添加堆排序

--- src/main/java/top/r2ys/sort/HeapSort.java
+++ src/main/java/top/r2ys/sort/HeapSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /**
  * @program: leetcode-course
  * @description: 堆排序，利用完全二叉树，大顶数
+ * https://www.cnblogs.com/MOBIN/p/5374217.html
  * https://github.com/r2ys/JS-Sorting-Algorithm/blob/master/7.heapSort.md
  * https://mp.weixin.qq.com/s?__biz=MzUyNjQxNjYyMg==&mid=2247484004&idx=1&sn=ecbafdec3c38ac7a13979aace18569e4&chksm=fa0e6de5cd79e4f3b059d507ac0c6bf9ec916711891f0e92377f0d4bcf9d24319d09ed68d990&scene=21#wechat_redirect
  * @author: HU
@@ -12,106 +13,45 @@ import java.util.Arrays;
  */
 public class HeapSort implements IArraySort {
 
-    //声明全局变量，用于记录数组array的长度；
-    static int len;
-    /**
-     * 堆排序算法
-     *
-     * @param array
-     * @return
-     */
-    public static int[] HeapSort(int[] array) {
-        len = array.length;
-        if (len < 1) return array;
-        //1.构建一个最大堆
-        buildMaxHeap(array);
-        //2.循环将堆首位（最大值）与末位交换，然后在重新调整最大堆
-        while (len > 0) {
-            swap(array, 0, len - 1);
-            len--;
-            adjustHeap(array, 0);
+    private static void heapSort(int[] arr) {
+        int len = arr.length -1;
+        for(int i = len/2 - 1; i >=0; i --){ //堆构造
+            heapAdjust(arr,i,len);
         }
-        return array;
-    }
-    /**
-     * 建立最大堆
-     *
-     * @param array
-     */
-    public static void buildMaxHeap(int[] array) {
-        //从最后一个非叶子节点开始向上构造最大堆
-        for (int i = (len - 1) / 2; i >= 0; i--) {
-            adjustHeap(array, i);
-        }
-    }
-    /**
-     * 调整使之成为最大堆
-     *
-     * @param array
-     * @param i
-     */
-    public static void adjustHeap(int[] array, int i) {
-        int maxIndex = i;
-        //如果有左子树，且左子树大于父节点，则将最大指针指向左子树
-        if (i * 2 < len && array[i * 2] > array[maxIndex])
-            maxIndex = i * 2;
-        //如果有右子树，且右子树大于父节点，则将最大指针指向右子树
-        if (i * 2 + 1 < len && array[i * 2 + 1] > array[maxIndex])
-            maxIndex = i * 2 + 1;
-        //如果父节点不是最大值，则将父节点与最大值交换，并且递归调整与父节点交换的位置。
-        if (maxIndex != i) {
-            swap(array, maxIndex, i);
-            adjustHeap(array, maxIndex);
+        while (len >=0){
+            swap(arr,0,len--);    //将堆顶元素与尾节点交换后，长度减1，尾元素最大
+            heapAdjust(arr,0,len);    //再次对堆进行调整
         }
     }
 
+    public static  void heapAdjust(int[] arr,int i,int len){
+        int left,right,j ;
+        while((left = 2*i+1) <= len){    //判断当前父节点有无左节点（即有无孩子节点，left为左节点）
+            right = left + 1;  //右节点
+            j = left;   //j"指针指向左节点"
+            if(j < len && arr[left] < arr[right])    //右节点大于左节点
+                j ++;     //当前把"指针"指向右节点
+            if(arr[i] < arr[j])    //将父节点与孩子节点交换（如果上面if为真，则arr[j]为右节点，如果为假arr[j]则为左节点）
+                swap(arr,i,j);
+            else         //说明比孩子节点都大，直接跳出循环语句
+                break;
+            i = j;
+        }
+    }
+    public static  void swap(int[] arr,int i,int len){
+        int temp = arr[i];
+        arr[i] = arr[len];
+        arr[len] = temp;
+    }
+    public static void main(String[] args) {
+        int array[] = {20,50,20,40,70,10,80,30,60};
+        System.out.println("排序之前:" + Arrays.toString(array));
+        heapSort(array);
+        System.out.println("排序之后:" + Arrays.toString(array));
+    }
 
     @Override
     public int[] sort(int[] sourceArray) throws Exception {
-        // 对 arr 进行拷贝，不改变参数内容
-        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
-
-        int len = arr.length;
-
-        buildMaxHeap(arr, len);
-
-        for (int i = len - 1; i > 0; i--) {
-            swap(arr, 0, i);
-            len--;
-            heapify(arr, 0, len);
-        }
-        return arr;
+        return new int[0];
     }
-
-    private void buildMaxHeap(int[] arr, int len) {
-        for (int i = (int) Math.floor(len / 2); i >= 0; i--) {
-            heapify(arr, i, len);
-        }
-    }
-
-    private void heapify(int[] arr, int i, int len) {
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        int largest = i;
-
-        if (left < len && arr[left] > arr[largest]) {
-            largest = left;
-        }
-
-        if (right < len && arr[right] > arr[largest]) {
-            largest = right;
-        }
-
-        if (largest != i) {
-            swap(arr, i, largest);
-            heapify(arr, largest, len);
-        }
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
 }
